a599c461ab2159493a34ad12a05f1ba2db889fee
Karol Siwek <siwek.karol1992@gmail.com>
2018-09-25 17:02:09 +0200

    zadanie

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x= 0;
+        do {
+            System.out.println("Podaj liczbę ");
+            x = scan.nextInt();
+            if (x > 200) {
+                System.out.println("Liczba jest za duża");
+            } else if (x < 100) {
+                System.out.println("Liczba jest za mała");
+            } else if (x % 3 != 0) {
+                System.out.println("Liczba nie jest podzielna przez 3");
+            }
+        } while (x < 200&&x > 100|| x % 3 != 0);
+        scan.close();
+    }
+}
